60d1a185f22e54b6cd534bda707b8188d2d13551
20181102931 <18647197975@163.com>
2019-10-16 10:23:00 +0800

    Initial Commit

---
+++ 骰子/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  骰子
+//
+//  Created by s20181102931 on 2019/10/16.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
