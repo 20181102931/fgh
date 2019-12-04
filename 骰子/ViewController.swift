dac46d634e108cd1cfa71611d42195b13a6e1b3e
20181102931 <18647197975@163.com>
2019-12-04 10:18:34 +0800

    23
    1

--- 骰子/ViewController.swift
+++ 骰子/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var oct: UIImageView!
+    @IBAction func button(_ sender: UIButton) {
+        let random2 = Int.random(in: 1...6)
+        picture.image = UIImage(named:"dice-\(random2)")
+        let random1 = Int.random(in:1...6)
+        oct.image = UIImage(named:"dice-\(random1)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func  motionBegan(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        let random2 = Int.random(in: 1...6)
+        picture.image = UIImage(named:"dice-\(random2)")
+        let random1 = Int.random(in:1...6)
+        oct.image = UIImage(named:"dice-\(random1)")
+    }
 
 }
 
